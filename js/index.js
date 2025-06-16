20996e23cec9398505dc68c5759b7138a6b21f0b
manishshukla00 <manishshuklacreative@outlook.com>
2025-06-16 11:52:47 +0530

    variables

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var a = "name";
+// dynimacally typed variable
+console.log(a);
+
+a = 10;
+// reassignedVariable
+console.log(a);
+
+let b = "name";
+// dynimacally typed variable
+console.log(b);
+
+b = 10;
+// reassignedVariable
+console.log(b);
+
+const c = "name";
+console.log(c);
+c = 10; // This will throw an error because 'c' is a constant
+console.log(c);
