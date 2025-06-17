4b8f9530cd8e9c1c27bb25a60d32870746149b52
manishshukla00 <manishshuklacreative@outlook.com>
2025-06-17 12:32:49 +0530

    array method

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 // var a = "name";
 // // dynimacally typed variable
 // console.log(a);
-
 // a = 10;
 // // reassignedVariable
 // console.log(a);
@@ -20,3 +19,44 @@
 // console.log(c);
 
 // variables scope ->
+
+// let x = 10; // block scope
+// {
+//   const y = 20; // block scope
+//   //   console.log(y); // 10
+// }
+// console.log(y);
+// console.log(typeof x);
+
+// function scope() {
+//   var a = 10; // function scope
+//   console.log(a);
+// }
+// console.log(a);
+
+// scope();
+
+// let a = 10; // global scope
+// a = 20;
+// console.log(a);
+
+// ${}
+// let name = "shami";
+// let age = 20;
+// console.log(`my name is ${name} and my age is ${age}`);
+
+// array
+// map method
+// const arr = [1, 2, 3, 4, 5];
+// const arr2 = arr.map((elem) => {
+//   return elem * 4;
+// });
+// console.log(arr, arr2);
+// arr.map(())
+
+// function add(a, b) {
+//   return a + b;
+// }
+// let add = (a, b) => a + b;
+
+// console.log(add(2, 8));
