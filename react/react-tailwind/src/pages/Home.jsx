876665878502dfc77cc445332f492f09df4b93c5
manishshukla00 <manishshuklacreative@outlook.com>
2025-06-30 12:49:03 +0530

    api intigration

--- react/react-tailwind/src/pages/Home.jsx
+++ react/react-tailwind/src/pages/Home.jsx
@@ -1,47 +1,39 @@
-import requests from "../Request";
+import { useState } from "react";
+import requests from "./netflixApi";
 import axios from "axios";
-import React, { useState, useEffect } from "react";
 
 const Home = () => {
-  const [urlData, setUrlData] = useState([]);
-  console.log(urlData);
+  const [netflixData, setNetflixData] = useState([]);
 
-  const getData = async () => {
+  const getData = () => {
     axios
-      .get(requests.requestPopular)
-      .then((res) => {
-        // console.log(res.data.results[0].id);
-        return setUrlData(res.data.results);
-      })
-      .catch((err) => {
-        console.error("Error fetching data:", err);
-      });
+      .get(requests.requestTopRated)
+      .then((res) => setNetflixData(res.data.results))
+      .catch((err) => console.log(err));
   };
-
-  useEffect(() => {
-    getData();
-  }, []);
 
   return (
     <>
-      {urlData.map((elem) => {
-        console.log(elem.backdrop_path);
-        return (
-          <div className="w-full h-screen flex flex-col bg-gray-800">
-            <img
-              key={elem.id}
-              src={`https://image.tmdb.org/t/p/w400/${elem?.backdrop_path}`}
-              alt=""
-            />
-          </div>
-        );
-      })}
       <button
-        className="p-4 bg-orange-950 text-white rounded-xl hover:bg-orange-700 transition-all duration-300"
         onClick={getData}
+        className="bg-amber-900 text-white p-2 rounded-2xl"
       >
         Get Data
       </button>
+      {netflixData.map((elem) => {
+        return (
+          <>
+            <div>
+              <img
+                className="w-full"
+                src={`https://image.tmdb.org/t/p/w500/${elem.backdrop_path}`}
+                alt="image"
+              />{" "}
+              1
+            </div>
+          </>
+        );
+      })}
     </>
   );
 };
